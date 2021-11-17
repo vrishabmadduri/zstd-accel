04c34bdd80d498a3da0d7423ed5e21717b62c04b
Cloud User <centos@ip-192-168-1-174.us-west-2.compute.internal>
2021-11-17 00:29:05 +0000

    made changes to logic

--- RLE/src/main/scala/rle-encode.scala
+++ RLE/src/main/scala/rle-encode.scala
@@ -24,7 +24,7 @@ class rleEncode()(implicit p: Parameters) extends Module
 val input_length = io.rle_stage_cmd.bits.rs1
 val input_pointer = io.rle_stage_cmd.bits.rs2
 val output_pointer = io.rle_encode_cmd.bits.rs1
-val encoded_length = io.rle_encode_cmd.bits.rs2
+val response_register = io.rle_encode_cmd.bits.rd // need to change rocc.h to account for rd
 
 // logic here
 
@@ -45,26 +45,25 @@ when (io.rle_encode_cmd.valid) {
 
 val request_fire = DecoupledHelper(
     io.l1helperUserRead.req.ready,
-    io.rle_stage_cmd.valid, // ???
+    io.rle_stage_cmd.valid,
     io.rle_encode_cmd.valid
 )
 
 val response_fire = DecoupledHelper(
-  io.l1helperUserWrite.resp.valid, // ???
-  byte_spitter.io.enq.ready,
-  // counter
+  io.l1helperUserWrite.resp.valid, 
+  byte_spitter.io.enq.ready
 )
 
 // counter for address translation/number of requests
 // 16-byte address aligned (enforce in software/assert in hardware)
 
-val addrinc = RegInit(UInt(0, 16.W))
+val addrinc = RegInit(UInt(0, 64.W))
 
-when (request_fire.fire() && (addrinc === 16.U)) {
+when (request_fire.fire() && (addrinc === input_length)) {
     addrinc := UInt(0)
-  } .elsewhen (request_fire.fire()) {
-    addrinc := addrinc + UInt(1)
-  }
+} .elsewhen (request_fire.fire()) {
+    addrinc := addrinc + UInt(16)
+}
 
 io.l1helperUserRead.req.bits.cmd := M_XRD
 io.l1helperUserRead.req.bits.size := 4.U
@@ -74,24 +73,25 @@ io.l1helperUserRead.req.bits.data := Bits(0)
 io.l1helperUserWrite.req.bits.cmd := M_XWR
 io.l1helperUserWrite.req.bits.size := 4.U
 
-io.rle_stage_cmd.ready := request_fire.fire(io.rle_stage_cmd.valid)
-io.rle_encode_cmd.ready := request_fire.fire(io.rle_encode_cmd.valid)
+io.rle_stage_cmd.ready := request_fire.fire(io.rle_stage_cmd.valid) && (addrinc === input_length)
+//io.rle_encode_cmd.ready := request_fire.fire(io.rle_encode_cmd.valid) && (addrinc === input_length) move after output is populated
 
-io.l1helperUserRead.req.bits.addr := input_pointer + (addrinc) //bitshift addrinc ???
+io.l1helperUserRead.req.bits.addr := input_pointer + (addrinc) 
 io.l1helperUserRead.req.valid := request_fire.fire(io.l1helperUserRead.req.ready)
 
 // need to pull input_pointer from L2/where it's stored and store in local val
 val input_val = io.l1helperUserRead.resp.bits.data // placeholder
-byte_spitter.io.enq.valid := request_fire.fire(byte_spitter.io.enq.ready)
+byte_spitter.io.enq.valid := response_fire.fire(byte_spitter.io.enq.ready)
+io.l1helperUserWrite.resp.ready := response.fire(io.l1helperUserWrite.resp.valid) && (byte_counter === 16)
 
 when(byte_spitter.io.enq.valid) {
   // increment the counter
-  byte_counter := byte_counter + 1
-}
+  byte_counter := byte_counter + 1.U
 
-if (byte_counter === 16) {
-  // pull in new data from L2
-  byte_counter := 0
+  when (byte_counter === 16.U) {
+    // pull in new data from L2
+    byte_counter := 0.U
+  }
 }
 
 byte_spitter.io.enq.bits := input_val(((byte_counter+1)<< 3 - 1), byte_counter << 3)
@@ -99,34 +99,25 @@ byte_spitter.io.enq.bits := input_val(((byte_counter+1)<< 3 - 1), byte_counter <
 val current_byte = RegInit(0.U(8.W))
 // create a register that keeps track of the previous byte
 val previous_byte = RegInit(0.U(8.W))
+val previous_byte_valid = RegInit(false.B)
 val output_val = RegInit(0.U(8.W))
 
 when(byte_spitter.io.deq.valid) {
-  if (previous_byte === 0.U) {
+  when (previous_byte_valid === false.B) {
     previous_byte := byte_spitter.io.deq.bits
-  } else {
+  } elsewhen {
     previous_byte := current_byte
   }
   current_byte := byte_spitter.io.deq.bits
 }
 
-//val start = RegInit(0.U(bit_width.W))
-//val end = RegInit(7.U(bit_width.W))
-//val encoded_msg = Vec(16, UInt(8.W))) // Bits?
 val i = RegInit(0.U(4.W)) // register
 val count = RegInit(0.U(4.W))
-val j = RegInit(0.U(4.W))
-when(i <= input_length - 1) {
+when(i < input_length) {
   count := 1.U
   ch = previous_byte
-  j := i
-  when(j < input_length - 1) {
-    if (previous_byte === current_byte) {
-      count := count + 1.U
-      j := j + 1.U // register
-      //start := start + 8.U // register
-      //end := end + 8.U // register
-    }
+  when (previous_byte === current_byte) {
+    count := count + 1.U
   }
   // add count, val to Queue
   // then cat count and val at the end and write to memory
@@ -135,8 +126,7 @@ when(i <= input_length - 1) {
   when(write_queue.io.enq.valid) { // FIXME: cat first
     write_queue.io.enq.bits := Cat(count, ch)
   }
-//encoded_msg := encoded_msg.fill() populate count and previous_byte every cycle
-  i := j + 1.U
+  i := i + 1.U
 }
 when(write_queue.io.deq.valid) { // FIXME: cat first
     output_val := write_queue.io.deq.bits
@@ -160,6 +150,3 @@ if (message[j] == message[j+1]):
         i = j+1
     return encoded_message
 */
-
-
-
